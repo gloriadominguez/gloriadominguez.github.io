710bc94acb8340b7ea5b78d039d8bb7377495517
Gloria R. Domínguez <128111097+gloriadominguez@users.noreply.github.com>
2023-03-18 00:13:56 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let typewriter = new Typewriter(app, {
  
 typewriter
   .pauseFor(2500)
-  .typeString('<span style="color: #b85351;">Desarrollo sitios web</span>')
+  .typeString('<span style="color: #7d93fc;">Desarrollo sitios web</span>')
   .pauseFor(200)
   .deleteChars(10)
   .start();
